3ed662b36aded7166a038df37a13ad05cd1bfe6b
Rishabh Raj <86614300+Raj-Rishabh@users.noreply.github.com>
2023-02-11 13:35:14 +0530

    Add files via upload

---
+++ fitness-club-registration/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import FitnessClubRegistration from "./App1";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<FitnessClubRegistration />);
